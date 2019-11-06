f7f6246b1e9e240ecf9c25d551fec34a4a251580
Vlad Vanyushin <vvanyushin@kameleoon.com>
2019-11-06 16:13:12 +0300

    added another some rules

--- code.js
+++ code.js
@@ -7,3 +7,27 @@ const age=10;
 var things = [ "asd" , "asdads" ];
 
 sayHi('alex');
+
+const items = new Array();
+
+console.log(items);
+
+[[0, 1], [2, 3], [4, 5]].reduce((acc, item, index) => {
+    const flatten = acc.concat(item);
+});
+
+const first = 1;
+const second = 1;
+
+if (first == second) {
+    console.log('equal');
+}
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+
+const foo = a && b < 0 || c > 0 || d + 1 === 0;
+
+console.log('foo', foo);
